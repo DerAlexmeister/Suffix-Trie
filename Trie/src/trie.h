efcd0410a624d67fc7635de977688111776e6167
DerAlexx <alexweb199@hotmail.de>
2019-01-03 16:36:50 +0100

    last version

--- Trie/src/trie.h
+++ Trie/src/trie.h
@@ -16,14 +16,66 @@
 template <class T, class E=char> class Trie {
 
 public:
+    class _node;
     class InternalNode;
-    InternalNode root ();
+    class Leaf;
     class trieIterator;
+    static InternalNode root;
     typedef std::basic_string <E> key_type;
     typedef std::pair<const key_type, T> value_type;
     typedef T mapped_type;
     typedef trieIterator iterator;
     const char lastChar = '#';
+
+    /**
+    * Method to return whether the Map isEmpty or not
+    */
+    bool empty(){
+        return root.empty();
+    }
+
+    /**
+    *   Insert a single InternalNode or Leaf.
+    */
+    iterator insert(const value_type& value) {
+        root.insert(value);
+    }
+
+    /**
+    *   Delete a single InternalNode or a Leaf.
+    */
+    void erase(const key_type& value) {
+        root.erase(value);
+    }
+
+    /**
+    * Method to clear the Tree.
+    * Except for the root.
+    */
+    void clear() {
+        try {
+            //root.clear();
+        } catch(...) {
+            std::cout << "an error occurred" << std::endl;
+        }
+    }
+
+    iterator lower_bound(const key_type& testElement);
+    iterator upper_bound(const key_type& testElement);
+    iterator find(const key_type& testElement);
+    iterator begin();
+    iterator end();
+
+    void showTrie() {
+
+    }
+
+    /**
+    * Method to generate a PAIR.
+    */
+    static std::pair<key_type, mapped_type> createPair(std::string word, std::string meaning) {
+        return std::make_pair<key_type, mapped_type>(word, meaning);
+    }
 
     class _node {
     public:
@@ -46,6 +98,8 @@ public:
     class InternalNode:public _node {
     public:
         std::map<E, _node*> mappyTheLittleMap;
+
+
         bool insert(const value_type& value) {
             try {
                 using namespace std;
@@ -116,13 +170,13 @@ public:
                     }
                     current = current.mappyTheLittleMap.find(currentChar).get<1>(value);
                 }
-                deleteNode->clear();
+                deleteNode -> clear();
             } else {
                 return false;
             }
 }
 
-        bool empty() {
+        bool empty() const{
             return mappyTheLittleMap.empty();
         }
 
@@ -130,59 +184,6 @@ public:
 
     class TrieIterator {
     };
-
-    /**
-    * Method to return whether the Map isEmpty or not
-    */
-    bool empty() const {
-        //return root.empty();
-        return true;
-    }
-
-    /**
-    *   Insert a single InternalNode or Leaf.
-    */
-    iterator insert(const value_type& value) {
-        root.insert(value);
-    }
-
-    /**
-    *   Delete a single InternalNode or a Leaf.
-    */
-    void erase(const key_type& value) {
-        root.erase(value);
-    }
-
-    /**
-    * Method to clear the Tree.
-    * Except for the root.
-    */
-    void clear() {
-        try {
-            //root.clear();
-        } catch(...) {
-            std::cout << "an error occurred" << std::endl;
-        }
-    }
-
-    iterator lower_bound(const key_type& testElement);
-    iterator upper_bound(const key_type& testElement);
-    iterator find(const key_type& testElement);
-    iterator begin();
-    iterator end();
-
-    void showTrie() {
-
-    }
-
-    /**
-    * Method to generate a PAIR.
-    */
-    static std::pair<key_type, mapped_type> createPair(std::string word, std::string meaning) {
-        return std::pair<key_type, mapped_type>(word, meaning);
-    }
-
-
 };
 
 #endif // TRIE_H_INCLUDED
